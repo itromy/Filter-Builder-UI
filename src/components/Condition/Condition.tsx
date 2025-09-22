c6a940c53d65dfb0e9c95af3e53e03f380f112cc
Romy Pohl <romy.pohl.rp@gmail.com>
2025-09-22 18:29:46 +0200

    feat(condition): add initial condition component

---
+++ src/components/Condition/Condition.tsx
@@ -0,0 +1,4 @@
+import classes from './Condition.module.css';
+export default function Condition() {
+  return <p className={classes.condition}>Condition</p>;
+}
--- src/components/Group/Group.tsx
+++ src/components/Group/Group.tsx
@@ -1,38 +1,51 @@
 import { useState, type JSX } from 'react';
 import { GroupOperators, type GroupOperatorType } from './GroupTypes';
 import classes from './Group.module.css';
+import Condition from '../Condition/Condition';
 export default function Group() {
   const [operator, setOperator] = useState<GroupOperatorType>(GroupOperators.And);
   const [childGroups, setChildGroups] = useState<JSX.Element[]>([]);
+  const [condtions, setConditions] = useState<JSX.Element[]>([]);
 
-  const addGroup = () => {
-    setChildGroups((prev) => [...prev, <Group key={prev.length} />]);
+  const render = () => {
+    return (
+      <div className={classes.group}>
+        <div className={classes.header}>
+          <select value={operator} onChange={changeGroupOperator}>
+            <option value={GroupOperators.And}>AND</option>
+            <option value={GroupOperators.Or}>OR</option>
+          </select>
+
+          <button onClick={addGroup}>Add Group</button>
+        </div>
+
+        <div>
+          <div className={classes.conditions}>
+            <button className={classes.addCondtionButton} onClick={addCondition}>
+              Add Conditon
+            </button>
+
+            {condtions}
+          </div>
+          {childGroups}
+        </div>
+      </div>
+    );
   };
 
-  const handleOperatorChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const changeGroupOperator = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const { value } = e.target;
 
     setOperator(value);
   };
 
-  return (
-    <div className={classes.group}>
-      <div className={classes.header}>
-        <select value={operator} onChange={handleOperatorChange}>
-          <option value={GroupOperators.And}>AND</option>
-          <option value={GroupOperators.Or}>OR</option>
-        </select>
+  const addGroup = () => {
+    setChildGroups((prev) => [...prev, <Group key={prev.length} />]);
+  };
 
-        <button onClick={addGroup}>Add Group</button>
-      </div>
+  const addCondition = () => {
+    setConditions((prev) => [...prev, <Condition key={prev.length} />]);
+  };
 
-      <div>
-        <div className={classes.conditions}>
-          <button>Add Conditon</button>
-        </div>
-
-        {childGroups}
-      </div>
-    </div>
-  );
+  return render();
 }
